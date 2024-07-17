4fae3b8ed36d0124ecb0381918da6acb9dd63fef
tranchinamit <48767069+tranchinamit@users.noreply.github.com>
2024-07-17 08:48:47 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "out",
-    emptyOutDir: true, // also necessary
-  },
+  // build: {
+  //   outDir: "out",
+  //   emptyOutDir: true, // also necessary
+  // },
 });
